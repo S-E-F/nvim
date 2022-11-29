291dd5be65d1179873658a1db690908c967588a6
Sef <severin.fitriyadi@gmail.com>
2022-11-29 21:55:24 +0100

    Omnisharp?

--- init.lua
+++ init.lua
@@ -1,2 +1,36 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 0
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.smarttab = true
+
+local ensure_packer = function()
+    local fn = vim.fn
+    local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+    if fn.empty(fn.glob(install_path)) > 0 then
+        fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+        vim.cmd [[packadd packer.nvim]]
+        return true
+    end
+    return false
+end
+
+vim.cmd([[
+    augroup packer_user_config
+        autocmd!
+        autocmd BufWritePost init.lua source <afile> | PackerCompile
+    augroup end
+]])
+
+local packer_boostrap = ensure_packer()
+
+return require('packer').startup(function(use)
+    use 'wbthomason/packer.nvim'
+    use 'OmniSharp/omnisharp-vim'
+    if packer_bootstrap then
+        require('packer').sync()
+    end
+end)
